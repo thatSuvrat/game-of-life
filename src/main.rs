0c63b69cf8760b7f994f2312909a83d605f027ac
Suvrat Bhatta <suvratbhatta@gmail.com>
2025-04-18 19:50:39 +0545

    feat: Added a bouncing ball UwU.
    Totally not me getting sidetracked ~_~

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Project: game_of_life
 use macroquad::prelude::*;
 
 // enum CellState {
@@ -7,8 +8,30 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Game of Life")]
 async fn main() {
-    let mut arr: [[i32; 100]; 100] = [[0; 100]; 100];
+    // let mut arr: [[i32; 100]; 100] = [[0; 100]; 100];
+    let mut pos: Vec2 = Vec2::new(screen_width()/2.0 , screen_height()/2.0);
+    let mut vel: Vec2 = Vec2::new(10.0, 1.0);
+    let g: f32 = -100.0;
+    let r: f32 = 10.0;
+
     loop {
-        clear_background(WHITE);
+        clear_background(LIGHTGRAY);
+        draw_circle(pos.x, pos.y, r, RED);
+
+        pos.x += vel.x * get_frame_time();
+        pos.y += vel.y * get_frame_time();
+
+        vel.y = vel.y - g * get_frame_time();
+
+        if pos.x > screen_width() || pos.x < 0.0 {
+            vel.x *= -1.0;
+        }
+        if pos.y > screen_height() || pos.y < 0.0 {
+            vel.y *= -1.0;
+        }
+
+        draw_text(&format!("X: {:.2} Y: {:.2}\nVelocity: ({:.2}, {:.2})", pos.x, pos.y, vel.x, vel.y), screen_width()/2.0, screen_height()/2.0, 12.0, BLACK);
+
+        next_frame().await;
     }
 }
