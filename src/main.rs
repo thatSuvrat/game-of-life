992cb51cf62f398e0a87afb19f929fd5eb58a27e
Suvrat Bhatta <suvratbhatta@gmail.com>
2025-04-18 18:23:54 +0545

    First Commit. Doesn't have anything implemented.
    Haiii :3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+// enum CellState {
+//     Alive,
+//     Dead,
+// }
+
+#[macroquad::main("Game of Life")]
+async fn main() {
+    let mut arr: [[i32; 100]; 100] = [[0; 100]; 100];
+    loop {
+        clear_background(WHITE);
+    }
 }
